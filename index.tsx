347d279a762ef1cc12b2485a8bc43849ca6c2f3d
TheShreekama <developer@shreekama.com>
2025-12-03 22:27:00 +0530

    move files to ignore

--- index.tsx
+++
@@ -1,5 +0,0 @@
-
-// This file is intentionally disabled.
-// The website has been converted to a static HTML/CSS/JS site.
-// The entry point is index.html, and logic is handled by js/script.js.
-console.log("Static site mode active.");
